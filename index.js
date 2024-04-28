122a812750387e376a66f367a5156d4c0791ad5e
alluri-reddy <136157062+alluri-reddy@users.noreply.github.com>
2024-04-28 20:00:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors(/*{
-  origin: "https://vasundara-real-estate.vercel.app",
-  credentials: true,
-}*/));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
